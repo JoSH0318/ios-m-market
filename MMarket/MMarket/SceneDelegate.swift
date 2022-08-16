8bddc2a4c09294cf3de316d6d9e6538d8ef4b67f
JoSH0318 <jsh9870@naver.com>
2022-08-16 22:56:38 +0900

    feat: MMarket project 생성 및 초기 설정

---
+++ MMarket/MMarket/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/08/16.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,  
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
---
+++ MMarket/MMarket/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
