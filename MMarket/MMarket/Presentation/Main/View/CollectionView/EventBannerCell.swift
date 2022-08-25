6ded79a8751af54af895f69d990bdf948d7efa7a
JoSH0318 <jsh9870@naver.com>
2022-08-25 22:22:44 +0900

    feat: class EventBannerCell 구현

---
+++ MMarket/MMarket/Presentation/Main/View/CollectionView/EventBannerCell.swift
@@ -0,0 +1,36 @@
+//
+//  EventBannerCell.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/08/17.
+//
+
+import SnapKit
+
+class EventBannerCell: UICollectionViewCell {
+    static var idenfier: String {
+        return String(describing: self)
+    }
+    
+    private let eventImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let eventImageStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private func configure() {
+        contentView.addSubview(eventImageStackView)
+        eventImageStackView.addArrangedSubview(eventImageView)
+        
+        eventImageStackView.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalTo(self.contentView)
+        }
+    }
+}
