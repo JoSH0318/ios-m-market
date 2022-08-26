06698a29ac4403fad838519663f7473bd2c87116
JoSH0318 <jsh9870@naver.com>
2022-08-26 22:06:59 +0900

    feat: MainViewController에 makeDataSource() 구현 및 dataSource 프로퍼티 생성

--- MMarket/MMarket/Presentation/Main/View/MainViewController.swift
+++ MMarket/MMarket/Presentation/Main/View/MainViewController.swift
@@ -16,11 +16,44 @@ class MainViewController: UIViewController {
     
     typealias DataSource = UICollectionViewDiffableDataSource<Section, Page>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Page>
+    private lazy var dataSource = makeDataSource()
     
     private let itemCollectionView = ItemCollectionView()
     private var pages = [Page]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// MARK: - DataSource
+
+extension MainViewController {
+    private func makeDataSource() -> DataSource {
+        let dataSource = DataSource(
+            collectionView: itemCollectionView
+        ) { collectionView, indexPath, itemIdentifier in
+            let sectionType = Section.allCases[indexPath.section]
+            
+            switch sectionType {
+            case .eventBanner:
+                guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: EventBannerCell.idenfier,
+                    for: indexPath
+                ) as? EventBannerCell else {
+                    return nil
+                }
+                return cell
+            case .main:
+                guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: ItemListCell.idenfier,
+                    for: indexPath
+                ) as? ItemListCell else {
+                    return nil
+                }
+                return cell
+            }
+        }
+        return dataSource
     }
 }
