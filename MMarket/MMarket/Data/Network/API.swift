63a125ad205b1e1dc9003beb5d44a4b524b85abe
JoSH0318 <jsh9870@naver.com>
2022-09-22 01:13:12 +0900

    feat: enum API, extension에 asEndpoint 프로퍼티 구현

--- MMarket/MMarket/Data/Network/API.swift
+++ MMarket/MMarket/Data/Network/API.swift
@@ -19,3 +19,64 @@ enum API {
     case scretKeySearch(Int)
     case productDelete(Int, String)
 }
+
+extension API {
+    var asEndpoint: Endpoint {
+        switch self {
+        case .productList(let pageNumber, let itemsPerPage):
+            return Endpoint(
+                baseURL: Constant.baseURL,
+                path: "/api/products",
+                method: .get,
+                queries: [
+                    "page_no": pageNumber,
+                    "items_per_page": itemsPerPage
+                ]
+            )
+        case .productDetail(let productID):
+            return Endpoint(
+                baseURL: Constant.baseURL,
+                path: "/api/products/\(productID)",
+                method: .get
+            )
+        case .productCreation(let body, let boundary):
+            return Endpoint(
+                baseURL: Constant.baseURL,
+                path: "/api/products",
+                method: .post,
+                header: [
+                    "Content-Type": "multipart/form-data; boundary=\(boundary)",
+                    "identifier": "8de44ec8-d1b8-11ec-9676-43acdce229f5"
+                  ],
+                body: body
+            )
+        case .productEdition(let body, let productID):
+            return Endpoint(
+                baseURL: Constant.baseURL,
+                path: "/api/products/\(productID)",
+                method: .patch,
+                header: [
+                    "Content-Type": "application/json",
+                    "identifier": "8de44ec8-d1b8-11ec-9676-43acdce229f5"
+                ],
+                body: body
+            )
+        case .scretKeySearch(let productID):
+            return Endpoint(
+                baseURL: Constant.baseURL,
+                path: "/api/products/\(productID)/secret",
+                method: .patch,
+                header: [
+                    "Content-Type": "application/json",
+                    "identifier": "8de44ec8-d1b8-11ec-9676-43acdce229f5"
+                ]
+            )
+        case .productDelete(let productID, let productSecret):
+            return Endpoint(
+                baseURL: Constant.baseURL,
+                path: "/api/products/\(productID)/\(productSecret)",
+                method: .delete
+            )
+        }
+    }
+}
