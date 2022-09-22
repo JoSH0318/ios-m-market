a96210fd19a4976097499c47dc98a68363080904
JoSH0318 <jsh9870@naver.com>
2022-09-23 04:01:52 +0900

    feat: DefaultProductRepository, generateMultiPartForm() 구현

--- MMarket/MMarket/Data/Repository/DefaultProductRepository.swift
+++ MMarket/MMarket/Data/Repository/DefaultProductRepository.swift
@@ -29,6 +29,36 @@ final class DefaultProductRepository: ProductRepository {
             .decode(type: ProductDTO.self, decoder: JSONDecoder())
             .map { $0.toEntity() }
     }
+extension DefaultProductRepository {
+    private func generateMultiPartForm(
+        by productRequest: ProductRequest,
+        _ images: [ImageFile],
+        _ boundary: String
+    ) -> Data? {
+        guard let jsonData = try? JSONEncoder().encode(productRequest) else { return nil }
+        var data = Data()
+        
+        data.appendString("\r\n--\(boundary)\r\n")
+        data.appendString("Content-Disposition: form-data; name=\"params\"\r\n")
+        data.appendString("Content-Type: application/json\r\n")
+        data.appendString("\r\n")
+        data.append(jsonData)
+        data.appendString("\r\n")
+        
+        for image in images {
+          data.appendString("--\(boundary)\r\n")
+          data.appendString("Content-Disposition: form-data; name=\"images\"; filename=\"\(image.fileName)\"\r\n")
+          data.appendString("Content-Type: image/\(image.type)\r\n")
+          data.appendString("\r\n")
+          data.append(image.data)
+          data.appendString("\r\n")
+        }
+        data.appendString("\r\n--\(boundary)--\r\n")
+        
+        return data
+    }
+}
+
 extension Data {
   mutating func appendString(_ string: String) {
     guard let data = string.data(using: .utf8) else {
