3e429880fa9daf80efb57e94ce87474086b01cc1
JoSH0318 <jsh9870@naver.com>
2022-10-05 00:24:13 +0900

    feat: MainViewModel, protocol을 활용한 In-Output 구조로 변경

--- MMarket/MMarket/Presentation/Main/ViewModel/MainViewModel.swift
+++ MMarket/MMarket/Presentation/Main/ViewModel/MainViewModel.swift
@@ -6,11 +6,47 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
 
-struct MainViewModel {
+protocol MainViewModelInput {}
+
+protocol MainViewModelOutput {
+    var sections: Observable<[ProductSectionModel]> { get }
+}
+
+protocol MainViewModelable: MainViewModelInput, MainViewModelOutput {}
+
+final class MainViewModel {
     private let productUseCase: ProductUseCase
+    private(set) var currentPage: Int = 1
     
     init(productUseCase: ProductUseCase) {
         self.productUseCase = productUseCase
+    }
+    
+    private func fetchProductList(pageNumber: Int, itemsPerPage: Int = 20) -> Observable<[Product]> {
+        return productUseCase
+            .fetchAllProducts(pageNumber: pageNumber, itemsPerPage: itemsPerPage)
+            .filter { $0.hasNext }
+            .do { self.currentPage = $0.pageNumber }
+            .map { $0.products }
+    }
+    
+    // MARK: - Output
+    
+    var sections: Observable<[ProductSectionModel]> {
+        return Observable
+            .combineLatest(
+                Observable.just([UIImage()]),
+                self.fetchProductList(pageNumber: currentPage)
+            )
+            .map { images, products in
+                var sections: [ProductSectionModel] = []
+                sections.append(.eventBanner(items: images))
+                sections.append(.productList(items: products))
+                
+                return sections
+            }
     }
 }
