5d8c48d180fe65e615cf39c0f976a194799d6d92
JoSH0318 <jsh9870@naver.com>
2022-10-25 17:11:00 +0900

    refactor: SceneDelegate, scene 메서드에 AppCoordinator 적용

--- MMarket/MMarket/Application/SceneDelegate.swift
+++ MMarket/MMarket/Application/SceneDelegate.swift
@@ -9,15 +9,25 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
+    var appCoordinator: AppCoordinator?
+    
     func scene(
         _ scene: UIScene,
-        willConnectTo session: UISceneSession,  
+        willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let navigationController = UINavigationController()
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        appCoordinator?.start()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) { }
 
     func sceneDidBecomeActive(_ scene: UIScene) { }
