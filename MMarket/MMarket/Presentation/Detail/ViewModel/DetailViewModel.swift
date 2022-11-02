ab9a96ad92c466a8450fab53f69bc3244c56e57b
JoSH0318 <jsh9870@naver.com>
2022-11-03 01:12:56 +0900

    feat: DetailViewModel 초기 구현

--- MMarket/MMarket/Presentation/Detail/ViewModel/DetailViewModel.swift
+++ MMarket/MMarket/Presentation/Detail/ViewModel/DetailViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
+protocol DetailViewModelInput {
+
+}
+
+protocol DetailViewModelOutput {
+
+}
+
+protocol DetailViewModelable: DetailViewModelInput, DetailViewModelOutput {}
+
+final class DetailViewModel: DetailViewModelable {
+    private let productUseCase: ProductUseCase
+    private let productID: Int
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Output
+    
+    var product: Observable<Product>?
+    
+    var productImagesURL: Observable<[String]?>?
+    
+    init(productUseCase: ProductUseCase, productID: Int) {
+        self.productUseCase = productUseCase
+        self.productID = productID
+        
+        self.fetchProduct(by: productID)
+    }
+    
+    
+    // MARK: - Input
+    
+    func fetchProduct(by productID: Int) {
+        productUseCase.fetchProduct(productId: productID)
+            .withUnretained(self)
+            .subscribe { viewModel, product in
+                viewModel.product = .just(product)
+                viewModel.productImagesURL = .just(product.images?.map { $0.url })
+            }
+            .disposed(by: disposeBag)
+    }
+}
