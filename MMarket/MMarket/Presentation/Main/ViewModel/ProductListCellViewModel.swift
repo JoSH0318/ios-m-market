fed2e6b11f2763edcc6176847ee42fceed5f3642
JoSH0318 <jsh9870@naver.com>
2022-11-04 15:14:37 +0900

    refactor: stock return 변경

--- MMarket/MMarket/Presentation/Main/ViewModel/ProductListCellViewModel.swift
+++ MMarket/MMarket/Presentation/Main/ViewModel/ProductListCellViewModel.swift
@@ -40,7 +40,7 @@ final class ProductListCellViewModel: ProductListCellViewModelable {
             return "품절"
         }
         
-        return "재고수량 " + formattedString(from: product.stock)
+        return "재고수량: " + "\(product.stock)"
     }
     
     var price: String {
