5b18143d0f51d63192e369e90f1538e5d247217f
JoSH0318 <jsh9870@naver.com>
2022-11-04 15:13:17 +0900

    feat: 등록버튼 추가 및 layout 구현

--- MMarket/MMarket/Presentation/Main/View/MainView.swift
+++ MMarket/MMarket/Presentation/Main/View/MainView.swift
@@ -13,15 +13,31 @@ final class MainView: UIView {
         collectionViewLayout: configureProductListLayout()
     )
     
+    let registerButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
+        button.tintColor = .systemBlue
+        button.backgroundColor = .systemBackground
+        button.clipsToBounds = true
+        return button
+    }()
+    
     init() {
         super.init(frame: .zero)
         
         configureLayout()
+        configureRegisterButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        registerButton.layer.cornerRadius = registerButton.frame.width * 0.5
+      }
     
     private func configureProductListLayout() -> UICollectionViewCompositionalLayout {
         return UICollectionViewCompositionalLayout { _, env in
@@ -59,5 +75,14 @@ final class MainView: UIView {
             ProductListCell.self,
             forCellWithReuseIdentifier: ProductListCell.idenfier
         )
+    }
+    
+    private func configureRegisterButton() {
+        addSubview(registerButton)
+        
+        registerButton.snp.makeConstraints {
+            $0.width.height.equalTo(50)
+            $0.trailing.bottom.equalTo(safeAreaLayoutGuide).offset(-20)
+        }
     }
 }
