06cca318e2028e7f4826df26f6c7d2645de85273
JoSH0318 <jsh9870@naver.com>
2022-11-04 21:57:00 +0900

    refactor: configureLayout() 구현

--- MMarket/MMarket/Util/DividerLineView.swift
+++ MMarket/MMarket/Util/DividerLineView.swift
@@ -11,14 +11,18 @@ final class DividerLineView: UIView {
     init(height: CGFloat) {
         super.init(frame: .zero)
         
+        configureLayout(height)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureLayout(_ height: CGFloat) {
         backgroundColor = .systemGray3
         self.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             heightAnchor.constraint(equalToConstant: height)
         ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
