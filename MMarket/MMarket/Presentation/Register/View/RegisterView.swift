9afc615bf871cfae4e2e689c5b848acaa9c06a96
JoSH0318 <jsh9870@naver.com>
2022-11-06 15:28:05 +0900

    refactor: registerView 구현

---
+++ MMarket/MMarket/Presentation/Register/View/RegisterView.swift
@@ -0,0 +1,186 @@
+//
+//  RegisterView.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/11/05.
+//
+
+import UIKit
+
+final class RegisterView: UIView {
+    
+    private enum FontSize {
+        static let title = 20.0
+        static let body = 16.0
+    }
+    
+    private let imageScrollView = UIScrollView()
+    
+    private let addImageButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "camera"), for: .normal)
+        button.tintColor = .darkGray
+        button.backgroundColor = .systemBackground
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    private let imageStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .white
+        textField.keyboardType = .default
+        return textField
+    }()
+    
+    private let currencySegmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["₩", "$"])
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.setContentCompressionResistancePriority(.required, for: .horizontal)
+        return segmentedControl
+    }()
+    
+    private let priceTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .white
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    private let discountedPriceTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .white
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    private let stockTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .white
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    private let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .white
+        textView.keyboardType = .default
+        textView.isScrollEnabled = false
+        textView.font = .systemFont(ofSize: 16)
+        return textView
+    }()
+    
+    private let priceStackView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
+    }()
+    
+    private let totalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    private let mainScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        
+        return scrollView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemBackground
+        configureLayout()
+        setContentsOfRegisterView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setContentsOfRegisterView() {
+        nameTextField.placeholder = "글 제목"
+        priceTextField.placeholder = "가격"
+        discountedPriceTextField.placeholder = "할인 가격"
+        stockTextField.placeholder = "재고 수량"
+    }
+    
+    func setProductRequest() -> ProductRequest? {
+        guard let name = nameTextField.text,
+              let price = Double(priceTextField.text!),
+              let discountedPrice = Double(descriptionTextView.text!)
+        else {
+            return nil
+        }
+        
+        var currency = "KRW"
+        
+        if currencySegmentedControl.selectedSegmentIndex == 1 {
+            currency = "USD"
+        }
+        
+        return ProductRequest(
+            name: name,
+            price: price,
+            discountedPrice: discountedPrice,
+            stock: Int(stockTextField.text!),
+            currency: currency,
+            descriptions: description,
+            secret: "xcnbof13rg2"
+        )
+    }
+    
+    private func configureLayout() {
+        addSubview(mainScrollView)
+        mainScrollView.addSubview(imageScrollView)
+        mainScrollView.addSubview(totalStackView)
+        
+        imageScrollView.addSubview(imageStackView)
+        imageStackView.addArrangedSubview(addImageButton)
+        
+        priceStackView.addArrangedSubview(priceTextField)
+        priceStackView.addArrangedSubview(currencySegmentedControl)
+        
+        totalStackView.addArrangedSubview(imageScrollView)
+        totalStackView.addArrangedSubview(nameTextField)
+        totalStackView.addArrangedSubview(priceStackView)
+        totalStackView.addArrangedSubview(discountedPriceTextField)
+        totalStackView.addArrangedSubview(stockTextField)
+        totalStackView.addArrangedSubview(descriptionTextView)
+        
+        mainScrollView.snp.makeConstraints {
+            $0.edges.equalTo(safeAreaLayoutGuide).offset(16)
+        }
+        
+        totalStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        imageScrollView.snp.makeConstraints {
+            $0.leading.top.trailing.equalToSuperview()
+            $0.height.equalTo(self.snp.height).multipliedBy(0.17)
+        }
+        
+        imageStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.height.equalToSuperview()
+        }
+        
+        addImageButton.snp.makeConstraints {
+            $0.height.equalToSuperview()
+            $0.width.equalTo(addImageButton.snp.height)
+        }
+    }
+}
