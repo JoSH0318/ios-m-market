0e90cd56f544b8549c3a5630157bc38dce3c69a3
JoSH0318 <jsh9870@naver.com>
2022-11-07 18:06:05 +0900

    refactor: ProductRequest 모델 변경

--- MMarket/MMarket/Domain/Entities/ProductRequest.swift
+++ MMarket/MMarket/Domain/Entities/ProductRequest.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 struct ProductRequest: Encodable {
-    let name: String
-    let price: Double
-    let discountedPrice: Double
+    let name: String?
+    let price: Int?
+    let discountedPrice: Int?
     let stock: Int?
-    let currency: String
-    let descriptions: String?
+    let currency: String?
+    let description: String?
     let secret: String
     
     private enum CodingKeys: String, CodingKey {
-        case name, price, stock, currency, descriptions, secret
+        case name, price, stock, currency, description, secret
         case discountedPrice = "discounted_price"
     }
 }
