d498cb0950c7ae0d734cf4e6380eda0cf3e6fe2f
JoSH0318 <jsh9870@naver.com>
2022-11-08 15:10:57 +0900

    feat: EditViewModel 초기 구현

---
+++ MMarket/MMarket/Presentation/Register/ViewModel/EditViewModel.swift
@@ -0,0 +1,81 @@
+//
+//  EditionViewModel.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/11/08.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+protocol EditViewModelInput {
+}
+
+protocol EditViewModelOutput {
+}
+
+protocol EditViewModelable: EditViewModelInput, EditViewModelOutput {}
+
+final class EditViewModel: EditViewModelable {
+    private let productUseCase: ProductUseCase
+    private let disposeBag = DisposeBag()
+    private let postRelay = PublishRelay<Void>()
+    private let imageRelay = BehaviorRelay<[Data]>(value: [])
+    private let errorRelay = ReplayRelay<Error>.create(bufferSize: 1)
+    
+    init(productUseCase: ProductUseCase) {
+        self.productUseCase = productUseCase
+    }
+    
+    // MARK: - Output
+    
+    var postProdct: Observable<Void> {
+        return postRelay.asObservable()
+    }
+    
+    var productImages: [Data] {
+        return imageRelay.value
+    }
+    
+    var imagesCount: Int {
+        return imageRelay.value.count
+    }
+    
+    var error: Observable<Error> {
+        return errorRelay.asObservable()
+    }
+    
+    // MARK: - Input
+    
+    func didTapPostButton(_ request: ProductRequest, images: [Data]) {
+        productUseCase.createProduct(with: request, images)
+            .withUnretained(self)
+            .subscribe { viewModel, _ in
+                
+            } onError: { error in
+                self.errorRelay.accept(error)
+            } onCompleted: {
+                self.postRelay.accept(())
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func selectImage(_ selectedImage: UIImage) {
+        let imageData = convertToImageFile(from: selectedImage)
+        var list = [Data]()
+        imageRelay.value.forEach { list.append($0)}
+        list.append(imageData)
+        imageRelay.accept(list)
+    }
+    
+    private func convertToImageFile(from image: UIImage) -> Data {
+        guard let imageData = image.jpegData(compressionQuality: 1) else {
+            return Data()
+        }
+        
+        return imageData
+    }
+}
+
+
