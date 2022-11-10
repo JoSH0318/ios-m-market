80b1cb2fa79cc6a3283d651f05f94a565808fcfc
JoSH0318 <jsh9870@naver.com>
2022-11-10 17:55:51 +0900

    feat: extension UIColor에 static 프로퍼티 구현

---
+++ MMarket/MMarket/Util/Extension/UIColor.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/11/10.
+//
+
+import UIKit
+
+extension UIColor {
+    static let grayColor = UIColor(named: "MainGrayColor")
+    static let navyColor = UIColor(named: "MainNavyColor")
+    static let beigeColor = UIColor(named: "MainBeigeColor")
+    static let balckColor = UIColor(named: "MainBalckColor")
+}
+
