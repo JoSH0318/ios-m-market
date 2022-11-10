4af908a99eb4783889d47fe72946d122e1f39723
JoSH0318 <jsh9870@naver.com>
2022-11-10 16:26:11 +0900

    feat: Font 추가 및 수정

---
+++ MMarket/MMarket/Util/ImageButton.swift
@@ -0,0 +1,69 @@
+//
+//  ImageAddButton.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/11/10.
+//
+
+import SnapKit
+
+final class ImageAddButton: UIButton {
+    private let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+        return stackView
+    }()
+    
+    private let addImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "camera")
+        imageView.tintColor = UIColor(named: "MainGrayColor")
+        return imageView
+    }()
+    
+    private let imageCountLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "BM HANNA Air TTF", size: 12)
+        label.textColor = UIColor(named: "MainGrayColor")
+        label.text = "0/5"
+        return label
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        
+        configureView()
+        configureConstraints()
+        configureButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setLabelText(with count: Int) {
+        imageCountLabel.text = String(count)
+    }
+    
+    private func configureButton() {
+        layer.borderWidth = 2
+        layer.cornerRadius = 8
+        layer.borderColor = UIColor(named: "MainGrayColor")?.cgColor
+        backgroundColor = .systemBackground
+        clipsToBounds = true
+    }
+    
+    private func configureView() {
+        addSubview(mainStackView)
+        mainStackView.addArrangedSubview(addImageView)
+        mainStackView.addArrangedSubview(imageCountLabel)
+    }
+    
+    private func configureConstraints() {
+        addImageView.snp.makeConstraints {
+            $0.height.width.equalToSuperview().multipliedBy(0.5)
+        }
+    }
+}
