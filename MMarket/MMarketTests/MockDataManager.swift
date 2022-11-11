e41f01e12e51ce05cd3cec4470549ba6b2919976
JoSH0318 <jsh9870@naver.com>
2022-11-11 16:53:16 +0900

    feat: MockDataManager 생성 및 구현

---
+++ MMarket/MMarketTests/MockDataManager.swift
@@ -0,0 +1,47 @@
+//
+//  MockDataManager.swift
+//  MMarketTests
+//
+//  Created by 조성훈 on 2022/11/11.
+//
+
+import UIKit
+
+struct MockDataManager {
+    func makeMockUrlSession() -> URLSession {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [MockURLProtocol.self]
+
+        return URLSession(configuration: configuration)
+    }
+
+    func makeRequestSuccessResult() {
+        MockURLProtocol.requestHandler = { _ in
+            let url = URL(string: "test")!
+            let httpResponse = HTTPURLResponse(
+                url: url,
+                statusCode: 200,
+                httpVersion: "2",
+                headerFields: nil
+            )!
+            let data = NSDataAsset(name: "MMarketSampleData")!.data
+
+            return (httpResponse, data)
+        }
+    }
+
+    func makeRequestFailureResult() {
+        MockURLProtocol.requestHandler = { _ in
+            let url = URL(string: "test")!
+            let httpResponse = HTTPURLResponse(
+                url: url,
+                statusCode: 400,
+                httpVersion: "2",
+                headerFields: nil
+            )!
+            let data = Data()
+
+            return (httpResponse, data)
+        }
+    }
+}
