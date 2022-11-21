90de3893b3eee647ca04cf6b06fd9955d18451a0
JoSH0318 <jsh9870@naver.com>
2022-11-21 23:34:16 +0900

    refactor: coordinator start 메서드에 tabBar 관련 로직 추가

--- MMarket/MMarket/Application/Coordinator/AppCoordinator.swift
+++ MMarket/MMarket/Application/Coordinator/AppCoordinator.swift
@@ -23,12 +23,36 @@ final class AppCoordinator: Coordinator {
     }
     
     func start() {
+        let tabBarController = UITabBarController()
+        tabBarController.tabBar.backgroundColor = .systemBackground
+        tabBarController.tabBar.tintColor = .label
+        
+        let mainNavigationController = UINavigationController()
+        mainNavigationController.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
+        mainNavigationController.navigationBar.tintColor = .label
+        
+        let myPageNavigationController = UINavigationController()
+        myPageNavigationController.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "star.fill"), tag: 1)
+        myPageNavigationController.navigationBar.tintColor = .label
+        
+        tabBarController.viewControllers = [mainNavigationController, myPageNavigationController]
+        navigationController.viewControllers = [tabBarController]
+        navigationController.setNavigationBarHidden(true, animated: false)
+        
         let mainCoordinator = MainCoordinator(
-            navigationController: self.navigationController,
+            navigationController: mainNavigationController,
             parentCoordinators: self,
             useCase: self.useCase
         )
-        self.childCoordinators.append(mainCoordinator)
+        let myPageCoordinator = MyPageCoordinator(
+            navigationController: myPageNavigationController,
+            parentCoordinators: self,
+            useCase: self.useCase
+        )
+        
+        childCoordinators.append(mainCoordinator)
+        childCoordinators.append(myPageCoordinator)
         mainCoordinator.start()
+        myPageCoordinator.start()
     }
 }
