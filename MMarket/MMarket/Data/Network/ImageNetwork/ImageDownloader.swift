28f75750495401439797f44a2cb327fda1e61123
JoSH0318 <jsh9870@naver.com>
2022-11-21 16:44:32 +0900

    refactor: Imagedownloader downloadImage 메서드를 requestImage로 변경

--- MMarket/MMarket/Data/Network/ImageNetwork/ImageDownloader.swift
+++ MMarket/MMarket/Data/Network/ImageNetwork/ImageDownloader.swift
@@ -9,7 +9,7 @@ import UIKit
 import RxSwift
 
 protocol ImageDownloaderType{
-    func downloadImage(_ urlString: String) -> Single<UIImage>
+    func requestImage(_ url: String, completion: @escaping (Result<UIImage, NetworkError>) -> Void) -> URLSessionDataTask?
 }
 
 final class ImageDownloader: ImageDownloaderType {
@@ -19,46 +19,43 @@ final class ImageDownloader: ImageDownloaderType {
         self.session = session
     }
     
-    func downloadImage(_ urlString: String) -> Single<UIImage> {
-        return Single<UIImage>.create { [weak self] single in
-            
-            guard let url = URL(string: urlString) else {
-                single(.failure(NetworkError.invalidURL))
-                return Disposables.create()
-            }
-            
-            let task = self?.session.dataTask(with: url) { data, response, error in
-                
-                guard error == nil else {
-                    single(.failure(NetworkError.errorIsOccurred(error.debugDescription)))
-                    return
-                }
-                
-                guard let response = response as? HTTPURLResponse,
-                      (200..<300).contains(response.statusCode)
-                else {
-                    single(.failure(NetworkError.invalidResponse))
-                    return
-                }
-                
-                guard let data = data else {
-                    single(.failure(NetworkError.invalidData))
-                    return
-                }
-                
-                guard let image = UIImage(data: data) else {
-                    single(.failure(NetworkError.invalidData))
-                    return
-                }
-                
-                single(.success(image))
-            }
-            task?.resume()
-            
-            return Disposables.create {
-                task?.suspend()
-                task?.cancel()
-            }
+    func requestImage(
+        _ url: String,
+        completion: @escaping (Result<UIImage, NetworkError>) -> Void
+    ) -> URLSessionDataTask? {
+        guard let url = URL(string: url) else {
+            completion(.failure(NetworkError.invalidURL))
+            return nil
         }
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            guard error == nil else {
+                completion(.failure(.errorIsOccurred(error.debugDescription)))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  (200..<300).contains(response.statusCode)
+            else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            completion(.success(image))
+        }
+        task.resume()
+        
+        return task
     }
 }
