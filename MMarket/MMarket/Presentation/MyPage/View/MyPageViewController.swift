c586b659c5587b4d02b9e17020f038ceb3d66d69
JoSH0318 <jsh9870@naver.com>
2022-11-21 23:33:30 +0900

    chore: MyPageVC, VM 파일 생성

---
+++ MMarket/MMarket/Presentation/MyPage/View/MyPageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MyPageViewController.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/11/21.
+//
+
+import UIKit
+
+final class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ MMarket/MMarket/Presentation/MyPage/ViewModel/MyPageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageViewModel.swift
+//  MMarket
+//
+//  Created by 조성훈 on 2022/11/21.
+//
+
+import Foundation
