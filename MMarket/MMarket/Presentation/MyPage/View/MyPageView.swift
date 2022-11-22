face271649ef31243cc4c37fd2a0debc7cb8fdf4
JoSH0318 <jsh9870@naver.com>
2022-11-22 18:12:40 +0900

    refactor: myPageView에 필요 없는 button 제거

--- MMarket/MMarket/Presentation/MyPage/View/MyPageView.swift
+++ MMarket/MMarket/Presentation/MyPage/View/MyPageView.swift
@@ -13,15 +13,6 @@ final class MyPageView: UIView {
         collectionViewLayout: configureProductListLayout()
     )
     
-    private(set) var registerButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
-        button.tintColor = UIColor.navyColor
-        button.backgroundColor = .systemBackground
-        button.clipsToBounds = true
-        return button
-    }()
-    
     init() {
         super.init(frame: .zero)
         
@@ -31,12 +22,6 @@ final class MyPageView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        registerButton.layer.cornerRadius = registerButton.frame.width * 0.5
     }
     
     private func configureProductListLayout() -> UICollectionViewCompositionalLayout {
@@ -68,7 +53,6 @@ final class MyPageView: UIView {
         backgroundColor = .systemBackground
         
         addSubview(productListCollectionView)
-        addSubview(registerButton)
         
         productListCollectionView.register(
             ProductListCell.self,
@@ -82,9 +66,6 @@ final class MyPageView: UIView {
             $0.leading.trailing.bottom.equalToSuperview()
         }
         
-        registerButton.snp.makeConstraints {
-            $0.width.height.equalTo(50)
-            $0.trailing.bottom.equalTo(safeAreaLayoutGuide).offset(-20)
         }
     }
 }
