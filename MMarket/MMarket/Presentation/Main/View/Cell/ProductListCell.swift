ac7068ee279537e69adf17ffb5cb55a941401bc5
JoSH0318 <jsh9870@naver.com>
2022-11-22 18:11:27 +0900

    refactor: productListCell Constraints 변경

--- MMarket/MMarket/Presentation/Main/View/Cell/ProductListCell.swift
+++ MMarket/MMarket/Presentation/Main/View/Cell/ProductListCell.swift
@@ -153,16 +153,15 @@ final class ProductListCell: UICollectionViewCell {
     
     private func configureConstraints() {
         thumbnailImageView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(8)
-            $0.leading.equalToSuperview().offset(16)
-            $0.trailing.equalToSuperview().offset(-16)
+            $0.leading.top.equalToSuperview().offset(8)
+            $0.trailing.equalToSuperview().offset(-8)
             $0.bottom.equalTo(mainStackView.snp.top).offset(-8)
             $0.width.equalTo(thumbnailImageView.snp.height)
         }
         
         mainStackView.snp.makeConstraints {
-            $0.leading.equalTo(contentView).offset(24)
-            $0.trailing.equalTo(contentView).offset(-24)
+            $0.leading.equalTo(contentView).offset(16)
+            $0.trailing.equalTo(contentView).offset(-16)
             $0.bottom.equalTo(contentView).offset(-8)
         }
     }
