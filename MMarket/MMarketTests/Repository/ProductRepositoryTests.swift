dea426e7967a60a2fc37fe296f3bb1c6a8e10b43
JoSH0318 <jsh9870@naver.com>
2022-11-30 22:14:56 +0900

    test: ProductRepositoryTests 메서드 추가

--- MMarket/MMarketTests/Repository/ProductRepositoryTests.swift
+++ MMarket/MMarketTests/Repository/ProductRepositoryTests.swift
@@ -61,36 +61,152 @@ final class ProductRepositoryTests: XCTestCase {
         self.disposeBag = nil
     }
     
-    func test_fetchProducts호출시_통신에_성공하면_올바른_ProductPage가_반환된다() throws {
+    func test_fetchProducts호출시_성공하면_ProductPage가_반환된다() throws {
         // given
+        let expectation = XCTestExpectation()
         let dummyProductPages = dummyProductPages
         
         // when
         sut.fetchProducts(with: 1, 20, "test")
-            .subscribe(onNext: { productpages in
+            .subscribe(onNext: { productPages in
                 // then
-                XCTAssertEqual(dummyProductPages, productpages)
-                XCTAssertEqual(productpages.products.count, 1)
+                XCTAssertEqual(productPages, dummyProductPages)
+                expectation.fulfill()
             }, onError: { _ in
                 XCTFail()
             })
             .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5.0)
     }
     
-    func test_fetchProduct호출시_통신에_성공하면_올바른_Product가_반환된다() throws {
+    func test_fetchProduct호출시_성공하면_Product가_반환된다() throws {
         // given
-        let dummyProduct = dummyProductPages.map { $0.products }!.first
+        let expectation = XCTestExpectation()
+        let dummyProduct =  ProductDTO(
+            id: 1,
+            venderId: 1,
+            name: "test",
+            description: "test",
+            thumbnail: "test",
+            currency: "test",
+            price: 1,
+            bargainPrice: 1,
+            discountedPrice: 1,
+            stock: 1,
+            images: nil,
+            vendor: nil,
+            createdAt: "test",
+            issuedAt: "test"
+        )
+        let dummyData = try! JSONEncoder().encode(dummyProduct)
+        networkProvider = StubNetworkProvider(data: dummyData, isSuccess: true)
+        sut = DefaultProductRepository(networkProvider: networkProvider)
         
         // when
         sut.fetchProduct(with: 1)
             .subscribe(onNext: { product in
                 // then
-                XCTAssertEqual(dummyProduct, product)
+                XCTAssertEqual(product, dummyProduct)
+                expectation.fulfill()
             }, onError: { _ in
                 XCTFail()
             })
             .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func test_createProduct호출시_성공하면_response를_반환한다() throws {
+        // given
+        let expectation = XCTestExpectation()
+        let productRequest = ProductRequest(
+            name: "test",
+            price: 1,
+            discountedPrice: 1,
+            stock: 1,
+            currency: "test",
+            description: "test",
+            secret: "test"
+        )
+        let data = Data()
+        
+        // when
+        sut.createProduct(with: productRequest, [data])
+            .subscribe(onNext: { _ in
+                // then
+                expectation.fulfill()
+            }, onError: { _ in
+                XCTFail()
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func test_patchProduct호출시_성공하면_response를_반환한다() {
+        // given
+        let expectation = XCTestExpectation()
+        let productRequest = ProductRequest(
+            name: "test",
+            price: 1,
+            discountedPrice: 1,
+            stock: 1,
+            currency: "test",
+            description: "test",
+            secret: "test"
+        )
+        
+        // when
+        sut.patchProduct(with: productRequest, 1)
+            .subscribe(onNext: {
+                // then
+                expectation.fulfill()
+            }, onError: { _ in
+                XCTFail()
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func test_searchDeleteURI호출시_성공하면_data를_반환한다() {
+        // given
+        let expectation = XCTestExpectation()
+        let dummyUri = "test"
+        let dummyData = try! JSONEncoder().encode(dummyUri)
+        networkProvider = StubNetworkProvider(data: dummyData, isSuccess: true)
+        sut = DefaultProductRepository(networkProvider: networkProvider)
+        
+        // when
+        sut.searchDeleteURI(with: "test", 1)
+            .map { try! JSONDecoder().decode(String.self, from: $0) }
+            .subscribe(onNext: { deleteUri in
+                // then
+                XCTAssertEqual(deleteUri, dummyUri)
+                expectation.fulfill()
+            }, onError: { _ in
+                XCTFail()
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func test_deleteProduct호출시_성공하면_response를_반환한다() {
+        // given
+        let expectation = XCTestExpectation()
+        
+        // when
+        sut.deleteProduct(with: "test")
+            .subscribe(onNext: {
+                // then
+                expectation.fulfill()
+            }, onError: { _ in
+                XCTFail()
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5.0)
     }
 }
-
-
